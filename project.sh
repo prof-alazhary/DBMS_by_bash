b27c820ba84735ba91689fa6092cc2f19f442024
mariamEraQi <mariam.eleraki@gmail.com>
2017-03-08 02:32:49 +0200

    DB
    using function ..
    still in progress ..

--- project.sh
+++ project.sh
@@ -1,4 +1,89 @@
 #!/bin/bash
+
+
+
+# createTable ----------------------------------------------
+createTableFun(){
+
+  echo "Enter Table name: "
+  read Tname;
+  touch $Tname
+  echo "Enter number of cols: "
+  read numcol;
+  for (( i = 0; i < numcol; i++ )); do
+    echo "Enter col name: ";
+    read colName;
+    names[$i]=$colName;
+  done
+  #----------------------
+  for (( i = 0; i < numcol; i++ )); do
+    #--- if problem
+    if [[ $i == $numcol-1 ]]; then
+      echo -n ${names[$i]}>>$Tname;
+    else
+      echo -n ${names[$i]}:>>$Tname;
+    fi
+
+  done
+
+}
+# delete -------------------------------------------------
+delete(){
+
+  select dchoice in deleteAll deleteRow  Exit
+  do
+    case $dchoice in
+      deleteAll)
+        echo "Enter Table name: "
+        read tableName
+        # echo "delete from $tableName where "
+        # read stmt
+        sed -i 'd' $tableName
+      ;;
+      deleteRow);;
+      Exit)
+        break 2;
+      ;;
+    esac
+  done
+}
+# UseDB ----------------------------------------------------
+useDB(){
+
+  echo "Enter Database name: "
+  read name;
+  cd $name
+  PS3='Select your choice number and press Enter: '
+  select Tchoice in createTable deleteRow dropTable Exit
+  do
+    case $Tchoice in
+
+    createTable)
+      createTableFun
+
+    ;;
+    #------------------------------------------------------
+    deleteRow)
+      delete
+    ;;
+    #------------------------------------------------------
+    dropTable)
+      echo "Enter Table Name: "
+      read name;
+      rm -f $name;
+    ;;
+    Exit)
+      break
+    ;;
+    *) print $REPLY is not one of the choices.
+    ;;
+
+    esac
+  done
+
+}
+#------------------------------------------------------------
+
 PS3='Select your choice number and press Enter: '
 select choice in createDB DropDB RenameDB UseDB Exit
 do
@@ -21,67 +106,9 @@ do
     read newName
     mv $oldName $newName
   ;;
-  ##################### UseDB ############################
+
   UseDB)
-    echo "Enter Database name: "
-    read name;
-    cd $name
-    PS3='Select your choice number and press Enter: '
-    select Tchoice in createTable deleteRow dropTable Exit
-    do
-      case $Tchoice in
-      #################createTable##########################
-      createTable)
-        #createTable;
-        echo "Enter Table name: "
-        read Tname;
-        touch $Tname
-        echo "Enter number of cols: "
-        read numcol;
-        for (( i = 0; i < numcol; i++ )); do
-          echo "Enter col name: ";
-          read colName;
-          names[i]=colName;
-        done
-        #----------------------
-        for (( i = 0; i < numcol; i++ )); do
-          echo names[i]:>>Tname;
-        done
-      ;;
-      #------------------------------------------------------
-      deleteRow)
-        select dchoice in deleteAll deleteRow  Exit
-        do
-          case $dchoice in
-            deleteAll)
-              echo "Enter Table name: "
-              read tableName
-              # echo "delete from $tableName where "
-              # read stmt
-              sed -i 'd' $tableName
-            ;;
-            deleteRow);;
-            Exit)
-              break 2;
-            ;;
-          esac
-        done
-      ;;
-      #------------------------------------------------------
-      dropTable)
-        echo "Enter Table Name: "
-        read name;
-        rm -f $name;
-      ;;
-      Exit)
-        break
-      ;;
-      *) print $REPLY is not one of the choices.
-      ;;
-
-      esac
-    done
-
+    useDB
   ;;
   #---------------------------------------------------------
   Exit)
@@ -92,10 +119,6 @@ do
   ;;
   esac
 done
+
 #in useDb check if not exist
 #-------------------------------------------------------------------------------
-# createTable(){
-#
-#
-#
-# }

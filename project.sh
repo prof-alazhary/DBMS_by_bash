6276e98e34b4aa91f458812a96ab1fc257c93c85
Azhary <prof.alazhary@gmail.com>
2017-03-09 23:31:55 +0200

    merge2

--- project.sh
+++ project.sh
@@ -2,30 +2,44 @@
 my_dir="$(dirname "$0")";
 source "$my_dir/insertTable.sh";
 
-
 # createTable ----------------------------------------------
 createTableFun(){
 
-  echo "Enter Table name: "
+  echo -n "Enter Table name: "
   read Tname;
   touch $Tname
-  echo "Enter number of cols: "
+  echo -n "Enter number of cols: "
   read numcol;
   for (( i = 0; i < numcol; i++ )); do
-    echo "Enter col name: ";
+    echo -n "Enter col name: ";
     read colName;
+    echo -n "Choose DataType integer as i or string as s: "
+    read dataT;
     names[$i]=$colName;
+    types[$i]=$dataT;
   done
-  #----------------------
+ echo "Done .."
+  # datatype--------------------------
   for (( i = 0; i < numcol; i++ )); do
-    #--- if problem
-    if [[ $i == $numcol-1 ]]; then
-      echo -n ${names[$i]}>>$Tname;
+
+    if [[ i -eq $numcol-1 ]]; then
+      echo  ${types[$i]}>>$Tname;
+    else
+      echo -n ${types[$i]}:>>$Tname;
+    fi
+
+  done
+  #-----------------------------------
+  for (( i = 0; i < numcol; i++ )); do
+
+    if [[ i -eq $numcol-1 ]]; then
+      echo  ${names[$i]}>>$Tname;
     else
       echo -n ${names[$i]}:>>$Tname;
     fi
 
   done
+
 
 }
 # insert -------------------------------------------------
@@ -49,21 +63,53 @@ delete(){
     case $dchoice in
       deleteAll)
         sed -i 'd' $tableName
+        echo "Deleted .. "
       ;;
       deleteRow)
         echo -n "Enter column name:  "
         read column
+        colOrder=$(awk -F: '{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$column'") print i}}}' $tableName)
         echo -n "Enter value:  "
         read value
         sed -i "/$value/d" $tableName
+        echo "Deleted .. "
       ;;
       Exit)
-        break 2;
+        return;
       ;;
     esac
   done
 }
 # Update----------------------------------------------------
+update(){
+
+  echo -n "Enter Table name: "
+  read tableName;
+
+  echo -n "Enter column name you need to update: "
+  read colU;
+  echo -n "Enter new value: "
+  read newValue;
+  colUNum=$(awk -F: '{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$colU'") print i}}}' $tableName)
+
+  echo -n "Enter column which will help to select value: "
+  read colS
+  echo -n "Enter its value: "
+  read svalue
+  colSnum=$(awk -F: '{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$colS'") print i}}}' $tableName)
+
+  svalueNum=$(awk -F: 'BEGIN{NF=='$colSnum'}{for(i=1;i<=NF;i++){if($i=="'$svalue'") {print NR; break}}}' $tableName)
+  echo $svalueNum;
+
+  oldValue=$(awk -v tmp="$colUNum" -F: '{if(NR=='$svalueNum'){print $tmp }}' $tableName)
+
+  sed -i 's/'$oldValue'/'$newValue'/' $tableName
+
+  #awk -v tmp="$colUNum" -F: '{if(NR=='$svalueNum'){$tmp="'$newValue'" }}' $tableName;
+
+
+
+}
 # select----------------------------------------------------
 selectData(){
   echo "Enter Table name: "
@@ -72,16 +118,18 @@ selectData(){
   do
     case $dchoice in
       selectAll)
-        #awk -F: "{print $0}" $tableName
-         awk -F: '{print $0}' $tableName
+         awk -F: '{if(NR>1)print $0}' $tableName
       ;;
       selectCol)
         echo -n "Enter column name:  "
         read column
-        awk  -v "/$value/d" $tableName
+
+        colOrder=$(awk -F: '{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$column'") print i}}}' $tableName)
+         awk -v tmp="$colOrder" -F: '{print $tmp}' $tableName;
+
       ;;
       Exit)
-        break 2;
+        return;
       ;;
     esac
   done
@@ -90,11 +138,17 @@ selectData(){
 # UseDB ----------------------------------------------------
 useDB(){
 
-  echo "Enter Database name: "
+  echo -n "Enter Database name: "
   read name;
-  cd $name
-  PS3='Select your choice number and press Enter: '
-  select Tchoice in createTable insertData deleteData SelectData dropTable Exit
+  if [[ -d "$name" ]]; then
+    cd $name;
+  else
+    echo "Database not found ..."
+    return;
+  fi
+
+  PS3='Select DB operation: '
+  select Tchoice in createTable insertData SelectData UpdateData deleteData dropTable Exit
   do
     case $Tchoice in
 
@@ -102,28 +156,33 @@ useDB(){
       createTableFun
 
     ;;
-    #------------------------------------------------------
+    #--------------------------------------
     insertData)
       insert
-    ;;
-    #------------------------------------------------------
-    deleteData)
-      delete
-    ;;
-    #------------------------------------------------------
+      ;;
+    #---------------------------------------
     SelectData)
       selectData
     ;;
-    #------------------------------------------------------
+    #---------------------------------------
+    UpdateData)
+      update
+    ;;
+    #---------------------------------------
+    deleteData)
+      delete
+    ;;
+    #---------------------------------------
     dropTable)
-      echo "Enter Table Name: "
+      echo -n "Enter Table Name: "
       read name;
       rm -f $name;
+      echo "Table have been removed"
     ;;
     Exit)
       break
     ;;
-    *) echo $REPLY is not one of the choices.
+    *) echo $REPLY "is not one of the choices".
     ;;
 
     esac
@@ -137,36 +196,40 @@ select choice in createDB DropDB RenameDB UseDB Exit
 do
   case $choice in
   createDB)
-    echo "Enter Database name: "
+    echo -n "Enter Database name: "
     read name
-    mkdir $name
-        ;;
+    if [[ -d "$name" ]]; then
+      echo "Database Already Exist ..."
+    else
+      mkdir $name;
+      break;
+    fi
+
+  ;;
   DropDB)
-    echo "Enter Database name: "
+    echo -n "Enter Database name: "
     read name
     rm  -r $name
-    echo "successfuly removed"
+    echo "successfuly removed .. "
   ;;
   RenameDB)
-    echo "Enter The old name: "
+    echo -n "Enter The old name: "
     read oldName
-    echo "Enter The new name: "
+    echo -n "Enter The new name: "
     read newName
     mv $oldName $newName
+    echo "Done .. "
   ;;
 
   UseDB)
     useDB
   ;;
-  #---------------------------------------------------------
+
   Exit)
+    echo "Bye :) .. "
     break
   ;;
 
-  *) echo $REPLY is not one of the choices.
+  *) echo $REPLY "is not one of the choices".
   ;;
   esac
-done
-
-#in useDb check if not exist
-#-------------------------------------------------------------------------------
